b67e37a11628346a92b3dc4497c3b0fb1e41e0e4
Stas Zosimov <zosimovv@yandex.ru>
2025-11-08 00:16:50 +0300

    0/3

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, profile, RegisterView
+from .views import home, profile, main_text, RegisterView
 
 urlpatterns = [
     path('', home, name='users-home'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
-    
+    path('main_text', main_text, name='main_text'),
+
    
 ]
